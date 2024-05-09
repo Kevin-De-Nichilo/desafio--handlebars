717e9860b403d7bcca03c55e18dbeb3047735b41
Kevin De Nichilo <kevin.palomino2002@gmail.com>
2024-05-09 14:26:29 -0300

    logger

--- src/app.js
+++ src/app.js
@@ -1,50 +1,81 @@
+//WINSTON
+
+//Los transportes pueden enviar información a: consola, base de datos y archivos.
+
+//Nivel: Sistema de prioridad que tiene cada log, para definir si lo enviamos a un archivo de texto o solo lo mostramos por consola.
+
+//Los niveles que ya vienen configurados con winston son los siguientes:
+
+{
+  error: 0;
+  warn: 1;
+  info: 2;
+  http: 3;
+  verbose: 4;
+  debug: 5;
+  silly: 6;
+}
+//Siendo el 0 el mas importante.
+
+//Lo voy a instalar con el siguiente comando: npm i winston
+
 const express = require("express");
 const app = express();
-const exphbs = require("express-handlebars");
-const cookieParser = require("cookie-parser");
-const passport = require("passport");
-const initializePassport = require("./config/passport.config.js");
-const cors = require("cors");
-const path = require("path");
 const PUERTO = 8080;
-require("./database.js");
-
-const productsRouter = require("./routes/products.router.js");
-const cartsRouter = require("./routes/carts.router.js");
-const viewsRouter = require("./routes/views.router.js");
-const userRouter = require("./routes/user.router.js");
+//Importamos el middleware:
+const addLogger = require("./utils/logger.js");
 
 //Middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-//app.use(express.static("./src/public"));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
+app.use(addLogger);
 
-//Passport
-app.use(passport.initialize());
-initializePassport();
-app.use(cookieParser());
+//Ruta
 
-//AuthMiddleware
-const authMiddleware = require("./middleware/authmiddleware.js");
-app.use(authMiddleware);
-
-//Handlebars
-app.engine("handlebars", exphbs.engine());
-app.set("view engine", "handlebars");
-app.set("views", "./src/views");
-
-//Rutas:
-app.use("/api/products", productsRouter);
-app.use("/api/carts", cartsRouter);
-app.use("/api/users", userRouter);
-app.use("/", viewsRouter);
-
-const httpServer = app.listen(PUERTO, () => {
-  console.log(`Servidor escuchando en el puerto ${PUERTO}`);
+app.get("/saludito", (req, res) => {
+  res.send("Olis!");
 });
 
-///Websockets:
-const SocketManager = require("./sockets/socketmanager.js");
-new SocketManager(httpServer);
+//Test
+
+app.get("/loggerTest", (req, res) => {
+  req.logger.error("Vamos a morir");
+  req.logger.warning("Cuidado! Hombre radiactivo!");
+  req.logger.info("Estamos navegando la app");
+
+  res.send("Logs generados!");
+});
+
+app.listen(PUERTO, () => {
+  console.log(`Escuchando en el puerto ${PUERTO}`);
+});
+
+//Artillery: es una herramienta que me permite simular múltiples peticiones de información en mi servidor, con la idea de testear el funcionamiento.
+
+//Lo instalamos de manera global: npm install -g artillery
+
+//Simulamos algunas operaciones para testear con Artillery:
+
+app.get("/operacionsimple", (req, res) => {
+  let suma = 0;
+  for (let i = 0; i < 1000000; i++) {
+    suma += i;
+  }
+
+  res.send({ suma });
+});
+
+//Operacion compleja:
+
+app.get("/operacioncompleja", (req, res) => {
+  let suma = 0;
+  for (let i = 0; i < 5e8; i++) {
+    suma += i;
+  }
+
+  res.send({ suma });
+});
+
+//Operacion simple:
+//artillery quick --count 40 --num 50 "http://localhost:8080/operacionsimple" -o simple.json
+
+//Operacion Compleja:
+//artillery quick --count 40 --num 50 "http://localhost:8080/operacioncompleja" -o compleja.json
